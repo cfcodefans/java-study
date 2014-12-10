1352883244d61dc7a85939090cf22afcb41c3d6d
fan <fan@thenetcircle.com>
2014-12-10 18:59:19 +0800

    m

--- src/cf/study/java8/javax/script/javascript/nashorn/NashornTests.java
+++ src/cf/study/java8/javax/script/javascript/nashorn/NashornTests.java
@@ -13,11 +13,13 @@ public class NashornTests {
 
 	public static void prepare(ScriptEngine se) throws Exception {
 		se.eval("function inspect(obj) { "
-				+ "print(typeof(obj) + ': ' + obj); "
-				+ "print('prototype: ' + obj.prototype);"
-				+ "print('json: ' + JSON.stringify(obj)); "
-				+ "print('length: ' + obj.length); "
-				+ "for (var p in obj) {print(p + ': ' + obj[p]);} "
+				+ "print(typeof(obj) + ': \t' + obj); "
+				+ "print('class: \t' + obj.class); "
+				+ "print('prototype: \t' + obj.prototype); "
+				+ "print('constructor: \t' + obj.constructor); "
+				+ "print('json: \t' + JSON.stringify(obj)); "
+				+ "print('length: \t' + obj.length); "
+				+ "for (var p in obj) {print(p + ': \t' + obj[p]);} "
 				+ "print();"
 				+ "}");
 		
@@ -184,7 +186,8 @@ public class NashornTests {
 		
 	}
 	
-	@Test public void workWithJavaArrays() throws Exception {
+	@Test 
+	public void workWithJavaArrays() throws Exception {
 		ScriptEngine se = sem.getEngineByName(NASHORN);
 		prepare(se);
 		
@@ -192,6 +195,184 @@ public class NashornTests {
 		se.eval("inspect(Java.type(\"long[]\"));");
 		se.eval("inspect(Java.type(\"float[]\"));");
 		se.eval("inspect(Java.type(\"java.lang.String[]\"));");
+		
+		se.eval("var IntArrayType = Java.type(\"int[]\");"
+				+ "var iArr = new IntArrayType(10); "
+				+ "inspect(iArr); "
+				+ "iArr[1] = 123; "
+				+ "iArr[2] = 321; "
+				+ "print(iArr[1] + iArr[2]); ");
+		
+		se.eval("var jsArr = ['a', 'b', 'c']; "
+				+ "inspect(jsArr); "
+				+ "var StrArrType = Java.type('java.lang.String[]'); "
+				+ "var javaStrArr =  Java.to(jsArr, StrArrType); "
+				+ "inspect(javaStrArr); "
+				+ "\n "
+//				+ "var _jsArr = Java.to(javaStrArr, Array); " //can't work reversely
+//				+ "inspect(_jsArr); "
+				+ " ");
+		
+		se.eval("for (var i = 0, j = iArr.length; i < j; i++) { "
+				+ "print(i + ':\t' + iArr[i]); }");
+		
+		se.eval("for (var i in iArr) { "
+				+ "print(iArr[i]); }");
+		
 	}
+	
+	@Test
+	public void workWithJavaString() throws Exception {
+		ScriptEngine se = sem.getEngineByName(NASHORN);
+		prepare(se);
+		
+		se.eval("var a = \"abc\"; "
+				+ "inspect(a); "
+				+ "var b = a + \"def\"; "
+				+ "inspect(b); "
+				+ "var c = String(b); "
+				+ "inspect(c); ");
+	}
+	
+	@Test
+	public void workWithNumbers() throws Exception {
+		ScriptEngine se = sem.getEngineByName(NASHORN);
+		prepare(se);
+		
+		se.eval("var intNum = 10; "
+				+ "inspect(intNum); "
+				+ "var dblNum = Number(intNum); "
+				+ "inspect(dblNum); ");
+	}
+	
+	@Test
+	public void workWithJavaCollections() throws Exception {
+		ScriptEngine se = sem.getEngineByName(NASHORN);
+		prepare(se);
+		
+		se.eval("var ArrayList = Java.type(\"java.util.ArrayList\"); "
+				+ "var aList = new ArrayList(); "
+				+ "inspect(aList); "
+				+ "aList.add(\"a\"); "
+				//+ "aList.push(\"b\"); " //though the ArrayList is treated as Array in Javascript, but it doesn't have Javascript Array's methods like push.....
+				+ "aList.add(\"c\"); "
+				+ "print(aList[1]); "
+				+ "for each (var e in aList) { "
+				+ "print(e);} print(); ");
+		
+		se.eval("var HashSet = Java.type(\"java.util.HashSet\"); "
+				+ "var _set = new HashSet(); "
+				+ "_set.add(\"a\"); "
+				+ "_set.add(\"b\"); "
+				+ "_set.add(\"c\"); "
+				+ "_set.add(\"a\"); "
+				+ "print(_set[2]); " //though the Set is treated as Array in Javascript, but it doesn't have index like Array
+				+ "inspect(_set); "
+				+ "for (var i in _set) {print(i);} "
+				+ "print(); "
+				+ "for each (var i in _set) {print(i);} "
+				+ "print(); "
+				+ " ");
+		
+		se.eval("var HashMap = Java.type(\"java.util.HashMap\"); "
+				+ "var _map = new HashMap(); "
+				+ "_map.put(\"name\", \"fan\"); "
+				+ "_map.put(\"height\", 1.75); "
+				+ "_map.put(\"weight\", 80); "
+				+ "print(_map[\"name\"]); " //This could work, but it isn't an Object
+				+ "inspect(_map); "); //
+	}
+	
+	@Test
+	public void forInAndForEachIn() throws Exception {
+		ScriptEngine se = sem.getEngineByName(NASHORN);
+		prepare(se);
+		
+		se.eval(" var arr = ['a', 'b', 'c', 'd', 'e']; "
+				+ "for (var i in arr) {print(i);} "
+				+ "print(); "
+				+ "for each (var i in arr) {print(i);} "
+				+ "print(); ");
+		
+		se.eval(" var point = {x: 100, y:200, z: 200}; "
+				+ "for (var i in point) {print(i);} "
+				+ "print(); "
+				+ "for each (var i in point) {print(i);} ");
+		
+	}
+	
+	@Test
+	public void inheritance() throws Exception {
+		ScriptEngine se = sem.getEngineByName(NASHORN);
+		prepare(se);
+		
+		Object obj = se.eval("var TestBean = Java.type(\"cf.study.java8.javax.script.javascript.nashorn.NashornTests.TestBean\"); \n "
+				+ " var JsTestBean = Java.extend(TestBean, { "
+				+ " jsObjMember : {}, "
+				+ " jsNumMember : 17, "
+				+ " jsArrMember : [], "
+				+ " jsMethodCallSuper : function() { "
+				+ " var _super_ = Java.super(this); "
+				+ " print('super.getPrivateMember: \t' + _super_.getPrivateMember()); "
+				+ " }, "
+				+ " toString : function() {return JSON.stringify(this);}, "
+				+ " jsMethodAccessSuper : function() {print('super.publicMember: \t' + this.publicMember);} "
+				+ " }); \n "
+				+ " inspect(JsTestBean); "
+				+ " var jsTestBean = new JsTestBean(); \n "
+				//+ " print(typeof(jsTestBean.jsMethodCallSuper)); \n " //jsMethodCallSuper disappears!
+				+ " jsTestBean.jsMethodCallSuper = function() { "
+				+ " var _super_ = Java.super(this); "
+				+ " print('super.getPrivateMember: \t' + _super_.getPrivateMember()); "
+				+ " }; \n"
+				+ " print(typeof(jsTestBean.jsMethodCallSuper)); "
+				//+ " print(\"super.getPrivateMember: \" + jsTestBean.jsMethodCallSuper()); \n "
+				+ " inspect(jsTestBean); "
+				+ " jsTestBean;");
+		
+		System.out.println(obj);
+		System.out.println(obj.getClass());
+		System.out.println((obj instanceof TestBean));
+	}
+	
+	@Test
+	public void implementsInterface() throws Exception {
+		ScriptEngine se = sem.getEngineByName(NASHORN);
+		prepare(se);
+		
+		// Create an object that implements the Runnable interface by implementing
+		// the run() method as a JavaScript function
+		se.eval("var r  = new java.lang.Runnable() { "
+		    + "run: function() { print('running...');} "
+			+ "};");
+
+		// The r variable can be passed to Java methods that expect an object implementing
+		// the java.lang.Runnable interface
+		se.eval("var th = new java.lang.Thread(r); "
+				+ " th.start(); "
+				+ " th.join(); ");
+		
+		// Define a JavaScript function
+		se.eval("function func() {print(\"I am func!\");};");
+		// Pass the JavaScript function instead of an object that implements
+		// the java.lang.Runnable interface
+		se.eval("var th = new java.lang.Thread(func); "
+				+ " th.start(); "
+				+ " th.join(); ");
+	}
+	
+	@Test
+	public void extendsAbstractClass() throws Exception {
+		ScriptEngine se = sem.getEngineByName(NASHORN);
+		prepare(se);
+		
+		se.eval("var TimerTask =  Java.type(\"java.util.TimerTask\");");
+		se.eval("var task = new TimerTask({ run: function() { print(\"Hello World!\") } });");
+		se.eval("var Timer = Java.type(\"java.util.Timer\");");
+		se.eval("var timer = new Timer();");
+		se.eval("timer.schedule(task, 1);");
+		
+	}
+	
 	
 }
