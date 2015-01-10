faf9da00d8d5b94cb873ae059f0197d79076e702
fan <cf.chenfan@gmail.com>
2015-01-10 18:51:05 +0800

    .

---
+++ src/cf/study/java8/search/lucene/LuceneTest.java
@@ -0,0 +1,17 @@
+package cf.study.java8.search.lucene;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+public class LuceneTest {
+
+	@BeforeClass
+	public void setUp() throws Exception {
+		
+	}
+	
+	@AfterClass
+	public void tearDown() throws Exception {
+		
+	}
+}
