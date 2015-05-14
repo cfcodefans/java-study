a80b5199ef555f4c9fe77359993c225d7199b1e9
fan <fan@thenetcircle.com>
2015-05-14 14:23:11 +0800

    .

---
+++ src/main/java/cf/study/misc/commons/cli/CommandLineTests.java
@@ -0,0 +1,15 @@
+package cf.study.misc.commons.cli;
+
+import org.junit.Test;
+
+public class CommandLineTests {
+	
+	@Test
+	public void testMain() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
