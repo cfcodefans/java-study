34cdeadd0a7a832616978469067e1221e5bb381f
cfcodefans <cf.chenfan@gmail.com>
2015-06-01 15:45:44 +0800

    .

--- src/main/java/cf/study/java8/lang/ProcessTests.java
+++ src/main/java/cf/study/java8/lang/ProcessTests.java
@@ -1,7 +1,11 @@
 package cf.study.java8.lang;
 
+import java.util.Map;
+import java.util.stream.Stream;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.PosixParser;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.SystemUtils;
 import org.junit.Test;
@@ -13,6 +17,24 @@ public class ProcessTests {
 		System.out.println(SystemUtils.JAVA_CLASS_PATH);
 		String[] classPaths = SystemUtils.JAVA_CLASS_PATH.split(SystemUtils.PATH_SEPARATOR);
 		System.out.println(StringUtils.join(classPaths, "\n"));
+	}
+	
+	@Test
+	public void testCallPython() throws Exception {
+		ProcessBuilder pb = new ProcessBuilder();
+		Process proc = pb.command("python2.7", "--version").start();
+		IOUtils.copy(proc.getInputStream(), System.out);
+		System.out.flush();
+	}
+	
+	@Test
+	public void testEnvVars() throws Exception {
+		ProcessBuilder pb = new ProcessBuilder();
+		Map<String, String> envVars = pb.environment();
+		envVars.entrySet().forEach(System.out::println);
+		
+		String path = envVars.get("Path");
+		Stream.of(StringUtils.split(path, ';')).map(String::trim).forEach(System.out::println);
 	}
 	
 	public static class JavaProcBuilder {
