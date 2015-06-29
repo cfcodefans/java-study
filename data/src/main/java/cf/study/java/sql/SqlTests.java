4b14268be9a935836fc9c872f53bb529ab011005
cfcodefans <cf.chenfan@gmail.com>
2015-06-29 10:16:15 +0800

    .

---
+++ data/src/main/java/cf/study/java/sql/SqlTests.java
@@ -0,0 +1,5 @@
+package cf.study.java.sql;
+
+public class SqlTests {
+
+}
--- java_8/src/main/java/cf/study/java8/lang/StringTests.java
+++ java_8/src/main/java/cf/study/java8/lang/StringTests.java
@@ -16,4 +16,11 @@ public class StringTests {
 		String str = "abc";
 		System.out.println(new String(str.getBytes(), "UTF-8"));
 	}
+	
+	@Test
+	public void testUnicode() throws Exception {
+		System.out.println((char)65);
+		System.out.println('\101'); // 8 * 8 + 1
+		System.out.println('\u0041'); //16 * 4 + 1
+	}
 }
