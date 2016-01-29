6718ecfdc94f27137d0a71bfa50e2e3fd3ff417d
cfcodefans <cf.chenfan@gmail.com>
2016-01-29 19:19:53 +0800

    zip

--- java_8/src/main/java/cf/study/java8/javax/tools/CompilerTests.java
+++ java_8/src/main/java/cf/study/java8/javax/tools/CompilerTests.java
@@ -14,10 +14,10 @@ import org.junit.Before;
 
 public class CompilerTests {
 
-	private JavaCompiler compiler;
-	
-	private StandardJavaFileManager javaSrcMgr;
-	
+	private JavaCompiler			compiler;
+
+	private StandardJavaFileManager	javaSrcMgr;
+
 	@Before
 	public void setUp() {
 		compiler = ToolProvider.getSystemJavaCompiler();
@@ -26,24 +26,32 @@ public class CompilerTests {
 		}
 		javaSrcMgr = compiler.getStandardFileManager(null, null, null);
 	}
-	
+
 	/**
 	 * compile given String source and return bytecodes as a Map.
 	 * 
-	 * @param fileName	source filename to be used for error message etc.
-	 * @param source	source java source as String
-	 * @param err		error writer where diagnostic message are written
-	 * @param srcPath	sourcePath locatoin of additional .java source files
-	 * @param clsPath	classPath location of additinal .class files
+	 * @param fileName
+	 *            source filename to be used for error message etc.
+	 * @param source
+	 *            source java source as String
+	 * @param err
+	 *            error writer where diagnostic message are written
+	 * @param srcPath
+	 *            sourcePath locatoin of additional .java source files
+	 * @param clsPath
+	 *            classPath location of additinal .class files
 	 * @return
 	 * @throws Exception
 	 */
 	private Map<String, byte[]> compile(String fileName, String source, Writer err, String srcPath, String clsPath) throws Exception {
-		//to collect errors, warnings etc.
+		// to collect errors, warnings etc.
 		DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
+
+		// create a new memory JavaFileManager
+		return null;
 	}
-	
+
 	public Method compileStaticMethod(final String methodName, final String clsName, final String source) throws Exception {
-		
+		return null;
 	}
 }
--- java_8/src/main/java/cf/study/java8/utils/zip/ZipTests.java
+++ java_8/src/main/java/cf/study/java8/utils/zip/ZipTests.java
@@ -1,38 +1,86 @@
 package cf.study.java8.utils.zip;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Optional;
+import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
+import java.util.zip.ZipOutputStream;
 
+
+
+
+
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.SystemUtils;
 import org.junit.Test;
-
 
 public class ZipTests {
 
 	public Path getSrcZipPath() {
 		return Paths.get(SystemUtils.JAVA_HOME).getParent().resolve("src.zip");
 	}
-	
+
 	@Test
 	public void loadZip() throws Exception {
 		System.out.println(SystemUtils.JAVA_HOME);
-		
+
 		Path zp = getSrcZipPath();
 		System.out.println(zp);
-		
+
 		try (ZipFile zf = new ZipFile(zp.toFile())) {
-			zf.stream().forEach(ze->System.out.println(ze.getName()));
-		} 
+			zf.stream().forEach(ze -> System.out.println(ze.getName()));
+		}
 		try (ZipFile zf = new ZipFile(zp.toFile())) {
-			Optional<? extends ZipEntry> zeOpt = zf.stream().filter(ze->!ze.isDirectory()).filter(ze->ze.getName().endsWith("java")).findFirst();
+			Optional<? extends ZipEntry> zeOpt = zf.stream().filter(ze -> !ze.isDirectory()).filter(ze -> ze.getName().endsWith("java")).findFirst();
 			if (zeOpt.isPresent()) {
 				InputStream is = zf.getInputStream(zeOpt.get());
 				System.out.println(IOUtils.toString(is));
+			}
+		}
+	}
+
+	@Test
+	public void makeZip() throws Exception {
+		Path p = Paths.get("src");
+		p.toFile().delete();
+
+		try (Stream<Path> paths = Files.walk(p, FileVisitOption.FOLLOW_LINKS)) {
+
+			try (FileOutputStream fos = new FileOutputStream("./test-output/src.zip")) {
+				try (ZipOutputStream zos = new ZipOutputStream(fos)) {
+					zos.setLevel(0);
+					// zos.setMethod(method);
+					paths.forEach((Path _p) -> {
+						File f = _p.toFile();
+						String zipPath = FilenameUtils.separatorsToUnix(_p.toString());
+						System.out.println(zipPath);
+						if (!f.isFile()) {
+							return;
+						}
+						try {
+							ZipEntry ze = new ZipEntry(zipPath);
+							zos.putNextEntry(ze);
+
+								byte[] fileBytes = FileUtils.readFileToByteArray(f);
+								zos.write(fileBytes);
+							zos.closeEntry();
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					});
+					zos.flush();
+				}
+				fos.flush();
 			}
 		}
 	}
