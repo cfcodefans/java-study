6c23d76def13f359052d79e48f08e8b799df79b4
fan <cf.chenfan@gmail.com>
2016-06-27 23:30:37 +0800

    .

--- misc/src/main/java/cf/study/testing/junit/RunWithTests.java
+++ misc/src/main/java/cf/study/testing/junit/RunWithTests.java
@@ -2,9 +2,10 @@ package cf.study.testing.junit;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-//@SuiteClasses(MathTests.class)
+@SuiteClasses(MathTests.class)
 public class RunWithTests {
 
 }
