5716d47d2cb096ae423dc94a3b2139c335d0d1db
fan <cf.chenfan@gmail.com>
2016-11-09 21:25:24 +0800

    ..

--- java_8/src/main/java/cf/study/java8/lang/ExceptionTests.java
+++ java_8/src/main/java/cf/study/java8/lang/ExceptionTests.java
@@ -15,6 +15,4 @@ public class ExceptionTests {
 
 	}
 
-	@Test
-	public
 }
---
+++ misc/src/main/java/cf/study/misc/quiz/QuizTests.java
@@ -0,0 +1,13 @@
+package cf.study.misc.quiz;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2016/9/30.
+ */
+public class QuizTests {
+	@Test
+	public void test1() {
+
+	}
+}
