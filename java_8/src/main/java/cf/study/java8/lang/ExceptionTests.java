700fd088cbe6561d877bb7938b87e0f1fb24630e
fan <cf.chenfan@gmail.com>
2016-11-15 00:26:23 +0800

    .

--- java_8/src/main/java/cf/study/java8/lang/ExceptionTests.java
+++ java_8/src/main/java/cf/study/java8/lang/ExceptionTests.java
@@ -12,7 +12,59 @@ public class ExceptionTests {
 		                     boolean writableStackTrace) {
 			super(message, cause, enableSuppression, writableStackTrace);
 		}
-
 	}
 
+	static void trigger() throws Exception {
+		throw new Exception("trigger");
+	}
+
+	static void wrap() throws Exception {
+		try {
+			trigger();
+		} catch (Exception e) {
+			throw new Exception("wrapper", e);
+		}
+	}
+
+	static void callSuppression(boolean enableSuppression) throws TestException {
+		try {
+			doubleTriggers();
+		} catch (Exception e) {
+			TestException te = new TestException("suppression: " + enableSuppression, e, enableSuppression, true);
+			te.addSuppressed(e);
+			throw te;
+		}
+	}
+
+	@Test
+	public void testSuppression() {
+		try {
+			callSuppression(true);
+		} catch (TestException te) {
+			te.printStackTrace();
+		}
+
+		System.out.println();
+
+		try {
+			callSuppression(false);
+		} catch (TestException te) {
+			te.printStackTrace();
+		}
+	}
+
+	static void doubleTriggers() throws Exception {
+		try {
+			throw new Exception("trigger");
+		} finally {
+			throw new Exception("trigger in finally");
+		}
+	}
+
+	static class ProblematicClosable implements AutoCloseable {
+
+		public void close() throws Exception {
+			throw new Exception("Exception in close");
+		}
+	}
 }
