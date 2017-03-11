d7320d55759a55363712187f126bde6be8564a7d
cfcodefans <cf.chenfan@gmail.com>
2017-03-12 00:18:29 +0800

    ..

---
+++ data/src/main/java/cf/study/java/algothrims/tree/TrieTests.java
@@ -0,0 +1,10 @@
+package cf.study.java.algothrims.tree;
+
+/**
+ * Created by fan on 2017/3/2.
+ */
+public class TrieTests {
+    static class DictTree {
+
+    }
+}
--- java_8/src/main/java/cf/study/java8/utils/concurrent/executors/ExecutorTests.java
+++ java_8/src/main/java/cf/study/java8/utils/concurrent/executors/ExecutorTests.java
@@ -8,56 +8,91 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ExecutorTests {
 
-	private static final Logger log = LogManager.getLogger(ExecutorTests.class);
+    private static final Logger log = LogManager.getLogger(ExecutorTests.class);
 
-	@Test
-	public void testSingleThreadExecutor() {
-		ExecutorService es = Executors.newSingleThreadExecutor();
-		es.submit(() -> {
-			while (!Thread.interrupted()) {
-				log.info("running");
-			}
-		});
+    @Test
+    public void testSingleThreadExecutor() {
+        ExecutorService es = Executors.newSingleThreadExecutor();
+        es.submit(() -> {
+            while (!Thread.interrupted()) {
+                log.info("running");
+            }
+        });
 
-		join();
-	}
+        join();
+    }
 
-	public static void join() {
-		try {
-			Thread.currentThread().join();
-		} catch (InterruptedException e) {
-			log.error("", e);
-		}
-	}
+    public static void join() {
+        try {
+            Thread.currentThread().join();
+        } catch (InterruptedException e) {
+            log.error("", e);
+        }
+    }
 
-	@Test
-	public void callExample() {
-		Callable<Date> something = () -> {
-			log.info("going to do something");
-			MiscUtils.easySleep(5000);
-			log.info("before I return the time");
-			return new Date();
-		};
+    @Test
+    public void callExample() {
+        Callable<Date> something = () -> {
+            log.info("going to do something");
+            MiscUtils.easySleep(5000);
+            log.info("before I return the time");
+            return new Date();
+        };
 
-		ExecutorService es = Executors.newCachedThreadPool();
-		List<Future<Date>> results = new ArrayList<Future<Date>>();
-		for (int i = 0; i < 10; i++) {
-			Future<Date> result = es.submit(something);
-			results.add(result);
-		}
+        ExecutorService es = Executors.newCachedThreadPool();
+        List<Future<Date>> results = new ArrayList<Future<Date>>();
+        for (int i = 0; i < 10; i++) {
+            Future<Date> result = es.submit(something);
+            results.add(result);
+        }
 
-		for (Future<?> result : results)
-			try {
-				log.info(result.get());
-			} catch (Exception e) {
-				log.error("", e);
-			}
-	}
+        for (Future<?> result : results)
+            try {
+                log.info(result.get());
+            } catch (Exception e) {
+                log.error("", e);
+            }
+    }
+
+    @Test
+    public void testSingleThreadScheduledExecutorSuppress() {
+        final ScheduledExecutorService ses = Executors.newSingleThreadScheduledExecutor();
+        final AtomicInteger cursor = new AtomicInteger(0);
+        Runnable sleepOnEven = () -> {
+            if (cursor.get() % 2 == 0) MiscUtils.easySleep(4000);
+            log.info("cursor: {}", cursor.getAndIncrement());
+        };
+        ses.scheduleAtFixedRate(sleepOnEven, 0, 1, TimeUnit.SECONDS);
+        MiscUtils.easySleep(10000);
+        ses.shutdownNow();
+    }
+
+    @Test
+    public void testSingleThreadScheduledExecutorException() {
+        final ScheduledExecutorService ses = Executors.newSingleThreadScheduledExecutor();
+        final AtomicInteger cursor = new AtomicInteger(0);
+        Runnable sleepOnEven = () -> {
+            if (cursor.get() == 5)
+                ses.shutdownNow();
+                //throw new RuntimeException("abort at 5");
+            log.info("cursor: {}", cursor.getAndIncrement());
+        };
+        ScheduledFuture<?> sf = ses.scheduleAtFixedRate(sleepOnEven, 0, 1, TimeUnit.SECONDS);
+        Object re = null;
+        try {
+            do {
+                re = sf.get();
+                log.info(String.format("receive %s from task", re));
+            } while (!(re instanceof Throwable));
+        } catch (Exception e) {
+            log.error(e);
+        } finally {
+            ses.shutdownNow();
+        }
+    }
 }
